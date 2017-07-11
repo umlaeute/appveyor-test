befe0a15291159ed26e55c85ed313a82d7af71ea
IOhannes m zmölnig <zmoelnig@iem.at>
2017-07-11 10:23:49 +0200

    string-define magic

--- foo.c
+++ foo.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAXPDSTRING 1000
+#ifndef PDBINDIR
+#define PDBINDIR "bin/"
+#endif
+
+#ifndef PDGUIDIR
+#define PDGUIDIR "tcl/"
+#endif
+
+#ifndef WISH
+# if defined _WIN32
+#  define WISH "wish85.exe"
+# else
+#  define WISH "wish"
+# endif
+#endif
+
 
 int main() {
 #ifdef __i386__
@@ -8,5 +27,12 @@ int main() {
   printf("amd64: ");
 #endif
   printf("hello world\n");
+
+  if(1) {
+    char wishbuf[MAXPDSTRING+30];
+    wishbuf[0]=0;
+    strcat(wishbuf, "/" PDBINDIR WISH);
+    printf("wish: %s\n", wishbuf);
+  }
   return 0;
 }
