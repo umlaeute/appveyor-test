746989093368aa6592c1ec942fff5a1febc05616
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-07-12 13:30:14 +0200

    re-enabled version-string
    but prefer _WIN64 over _WIN32 since the latter is always defined

--- test.c
+++ test.c
@@ -1,14 +1,12 @@
-#if 0
 #define MY_VERSION_STRING "Frobnozzel v0.1; (c) 2019 ACME; GPLv3 "
 #if defined(__GNUC__)
     asm(".ascii \"" MY_VERSION_STRING "\"\n\t");
 #elif defined(_MSC_VER)
-# if defined(_WIN32)
-    __asm db MY_VERSION_STRING
-# elif defined(_WIN64)
+# if defined(_WIN64)
 #  pragma comment(user, MY_VERSION_STRING
+# elif defined(_WIN32)
+    __asm db MY_VERSION_STRING
 # endif
-#endif
 #endif
 
 #include <stdio.h>
